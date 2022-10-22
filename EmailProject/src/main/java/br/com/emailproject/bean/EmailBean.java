3c01e41337e1370b760ff55984e205a809766443
Alex <Alex@DESKTOP-NDEJR7S.Dlink>
2022-10-22 09:09:02 -0300

    Fim AULA 19.16 - Layout do e-mail

--- EmailProject/src/main/java/br/com/emailproject/bean/EmailBean.java
+++ EmailProject/src/main/java/br/com/emailproject/bean/EmailBean.java
@@ -16,6 +16,10 @@ public class EmailBean implements Serializable {
 
 	private static final long serialVersionUID = 4538755582654584073L;
 	
+	private static final String DESTINATARIO = "tadeu@gmail.com";
+	
+	private static final String ASSUNTO = "Mudança de senha";
+	
 	@Inject
 	private EmailService emailService;
 	
@@ -26,7 +30,7 @@ public class EmailBean implements Serializable {
 	
 	private Email montarEmail() {
 		Emaillayout layout = new Emaillayout();
-		return layout.montarEmailAdministrador("tadeu@gmail.com", "Mudança de senha");
+		return layout.montarEmailAdministrador(DESTINATARIO, ASSUNTO);
 	}
 
 }
--- EmailProject/src/main/java/br/com/emailproject/dto/Emaillayout.java
+++ EmailProject/src/main/java/br/com/emailproject/dto/Emaillayout.java
@@ -3,12 +3,62 @@ package br.com.emailproject.dto;
 import br.com.emailproject.model.Email;
 
 public class Emaillayout {
+	
+private static final String QUEBRA_DE_LINHA_DUPLA = "<br><br>";
+
+private static final String QUEBRA_DE_LINHA_UNICA = "<br>";
 
 	public Email montarEmailAdministrador(String destinatario, String assunto) {
 		
 		StringBuilder texto = new StringBuilder();
 		
+		texto
+			.append("A/C Adminstrador")
+			.append(QUEBRA_DE_LINHA_DUPLA);
+		
+		texto
+		.append("Solicito alteração de senha do sistema!")
+		.append(QUEBRA_DE_LINHA_DUPLA);
+		
+		gerarAssinatura(texto);
+		
+		gerarRodape(texto);
+		
 		return new Email(destinatario, assunto, texto.toString());
+		
+	}
+	
+	public Email montarEmailSecretario(String destinatario, String assunto) {
+		
+		StringBuilder texto = new StringBuilder();
+		
+		texto
+			.append("A/C Secretário")
+			.append(QUEBRA_DE_LINHA_DUPLA);
+		
+		texto
+		.append("Texto personalizado para secretário!")
+		.append(QUEBRA_DE_LINHA_DUPLA);
+		
+		gerarAssinatura(texto);
+		
+		gerarRodape(texto);
+		
+		return new Email(destinatario, assunto, texto.toString());
+		
+	}
+	
+	private String gerarAssinatura(StringBuilder texto) {
+		return texto
+				.append("Att.:")
+				.append(QUEBRA_DE_LINHA_UNICA)
+				.append("Operador de Caixa")
+				.append(QUEBRA_DE_LINHA_DUPLA)
+				.toString();
+	}
+
+	private String gerarRodape(StringBuilder texto) {
+		return texto.append("E-mail automático. Favor não responder esse e-mail!").toString();
 		
 	}
 	
